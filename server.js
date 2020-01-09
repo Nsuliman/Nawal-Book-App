451e04d3476ffa1846a9d74c943e9cf5f157c297
Nsuliman <nsuliman88@gmail.com>
2020-01-10 01:28:34 +0200

    Trying with update , almost done but the selected book not updated but added new copy , keep trying

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
-const pg = require('pg');                           
+const pg = require('pg');
 const methodOverride = require('method-override')
 const expressLayouts = require('express-ejs-layouts')
 
@@ -17,16 +17,14 @@ const server = express();
 server.use(cors());
 
 // DataBase
-
-
 const client = new pg.Client(process.env.DB_URL);
-client.on('error',err => console.error(err));
+client.on('error', err => console.error(err));
 
 server.use(express.json());
 server.use(express.static('./public'));
 server.use(express.urlencoded({ extended: true }));
 server.set('view engine', 'ejs');
-// server.use(methodOverride(middleware));
+server.use(methodOverride(middleware));
 // server.use(expressLayouts);
 
 
@@ -48,9 +46,9 @@ server.get('/new', (req, res) => {
 });
 
 //Restore Data From DataBase 
-server.get('/', (req,res) =>
-{
+server.get('/', (req, res) => {
     let SQL = `SELECT * FROM books `
+    // // console.log('SQL : ', SQL);
     client.query(SQL)
         .then(data => {
             res.render('pages/index', { books: data.rows })
@@ -67,7 +65,7 @@ server.post('/searches', (req, res) => {
     if (req.body.searchtype === 'title') {
         url = url + req.body.search;
         // console.log(' Title url : \n\n\n\n\n\n ', url);   
-     }
+    }
     else if (req.body.searchtype === 'author') {
         url = url + req.body.search;
         // console.log(' Author url : \n\n\n\n\n\n', url);   
@@ -78,14 +76,13 @@ server.post('/searches', (req, res) => {
             // console.log('dataaaaaaaaaaaaaaaaaaaaaaaaaaaaa Boddddddddddddddy : ', data.body);
             // console.log('dataaaaaaaaaaaaaaaaaaaaaaaaaaaaa Itemmmmmmmmmmmmmmmms : ', data.body.items);
             let arr = data.body.items;
-            let books = arr.map(book =>
-                {   
-                    // console.log('book : ', book);
-                    return new Book(book);
-                });
+            let books = arr.map(book => {
+                // console.log('book : ', book);
+                return new Book(book);
+            });
             // let listBooks = data.body.items;
             // console.log('books : \n\n\n\n\n', books);
-            res.render('pages/books/show', {books: books})
+            res.render('pages/books/show', { books: books })
         })
         .catch(error => {
             // console.log('Errorrrrrrrrrrrr : ', error);
@@ -95,10 +92,11 @@ server.post('/searches', (req, res) => {
 
 
 // View Details
-server.get('/books/:books_id',(req,res) =>
-{
+server.get('/books/:books_id', (req, res) => {
     let SQL = `SELECT * FROM books WHERE id=$1`
+    // // console.log('SQL : ', SQL);
     let values = [req.params.books_id]
+    // console.log('req.params.books_id : ', req.params.books_id);
 
     client.query(SQL, values)
         .then(results => {
@@ -107,18 +105,21 @@ server.get('/books/:books_id',(req,res) =>
 });
 
 // Add Book To DataBase 
-server.post('/add', (req,res) =>
-{
-    res.render('pages/serches/add',{books:req.body});
+server.post('/add', (req, res) => {
+    // console.log('req.body : ', req.body);
+
+    res.render('pages/serches/add', { books: req.body });
 });
 
-
-server.post('/books', (req,res) =>
-{
+// Save Book Details into DataBase then redirect me to the homePage with All saved books in DB 
+server.post('/books', (req, res) => {
+    // console.log('req.body : ', req.body);
     let { image, title, author, des, isbn, bookshelf } = req.body
 
     let SQL = `INSERT INTO books (image, title, author, des, isbn, bookshelf) VALUES ($1, $2, $3, $4, $5, $6)`
+    // // console.log('SQL : ', SQL);
     let values = [image, title, author, des, isbn, bookshelf]
+    // console.log('values : ', values);
 
     client.query(SQL, values)
         .then(() => {
@@ -126,6 +127,29 @@ server.post('/books', (req,res) =>
         })
 });
 
+// Update book details by clicking and show up the form to be able to update 
+server.post('/update', (req, res) => {
+    // console.log('req.body : ', req.body);
+    res.render('pages/books/edit', { books: req.body });
+})
+
+// After finished update save the updated details into DB then redirect me to Homepage 
+server.put('/update/:books_id', (req, res) => {
+
+    let { image, title, author, des, isbn, bookshelf } = req.body;
+    // // console.log('req.body : ', req.body);
+
+    let SQL = `UPDATE books SET image=$1, title=$2, author=$3, des=$4, isbn=$5, bookshelf=$6 WHERE id=$7`
+    // console.log('SQL : ', SQL);
+
+    let values = [image, title, author, des, isbn, bookshelf, req.params.books_id];
+    // console.log('values : ', values);
+    
+    client.query(SQL, values)
+        .then(() => {
+            res.redirect('/')
+        })
+});
 
 // If you want to use Constructor function you must use the keys names inside the ejs file
 // Otherwise just pass the data inside the object of the render line {anyname: data Path} , "anyname" use same name in the EJS file for foreach
@@ -136,13 +160,20 @@ function Book(data) {
     this.title = data.volumeInfo.title;
     this.isbn = (data.volumeInfo.industryIdentifiers && data.volumeInfo.industryIdentifiers[0].identifier) || ' No ISBN Found';
     this.image = (data.volumeInfo.imageLinks && data.volumeInfo.imageLinks.thumbnail) || ' No Image Found ';
-    this.des = (data.volumeInfo.description && data.volumeInfo) || ' No Description Found ' ;
+    this.des = (data.volumeInfo.description && data.volumeInfo) || ' No Description Found ';
 } // End of location constructor function 
 
+// For update and Delete , build-in function as it is .
+function middleware(req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        let method = req.body._method;
+        delete req.body._method;
+        return method;
+    }
+} // end of middleware function 
 
 /******************************* Server Listening  ***********************************/
 client.connect()
-.then( () => 
-{
-    server.listen(PORT, () => console.log('New Book App , listening On port # : ', PORT));
-});
+    .then(() => {
+        server.listen(PORT, () => console.log('New Book App , listening On port # : ', PORT));
+    });
